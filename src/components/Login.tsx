6e6b1897d0c828d95f340c977caac2d050bbbeef
vinischmitt <118611384+vinischmitt@users.noreply.github.com>
2023-09-08 20:00:22 -0300

    1 file modified

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 
     export function Login(){
 
-      const [usuario, setUsuario]= useState();
-      const [senha, setSenha]= useState();
+      const [usuario, setUsuario]= useState('');
+      const [senha, setSenha]= useState('');
 
       return (
         <>
         <form>
           <div>
              <label>Usuário:</label>
-              <input />
+              <input onChange={(e) => setUsuario(e.target.value)} value={usuario}/>
           </div>
           <div>
              <label>Senha:</label>
-             <input type="password" />
+             <input  onChange={(e) => setUsuario(e.target.value)} value={senha} type="password" />
           </div>
         </form>
         </>
