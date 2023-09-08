3da71283612807b50b598570991123abe4491a1c
vinischmitt <118611384+vinischmitt@users.noreply.github.com>
2023-09-08 19:49:52 -0300

    2 files modified, 1 added

---
+++ src/components/Login.tsx
@@ -0,0 +1,16 @@
+    export function Login(){
+      return (
+        <>
+        <form>
+          <div>
+             <label>Usuário:</label>
+              <input />
+          </div>
+          <div>
+             <label>Senha:</label>
+             <input type="password" />
+          </div>
+        </form>
+        </>
+      );
+    } 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import { Login } from '../components/Login';
 
 export function Login() {
   return (
     <>
       <h1>Login</h1>
+      <Login />
       <Link to="/">Voltar para Home</Link>
 
     </>
