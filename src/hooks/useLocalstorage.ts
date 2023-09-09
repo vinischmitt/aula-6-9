9972fb86e5d405474c13b243b5788e6b6cabe612
vinischmitt <118611384+vinischmitt@users.noreply.github.com>
2023-09-08 21:54:10 -0300

    1 file modified

--- src/hooks/useLocalstorage.ts
+++ src/hooks/useLocalstorage.ts
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function (key:string, defaultValue: unknown) {
-  const [value, setvalue] = useState(() => {
-    try {
+export default function(key:string, defaultValue: unknown) {
+  const [value, setValue] = useState(()=> {
+    try{
       return JSON.parse(localStorage.getItem(key) || String(defaultValue));
-    } catch (err) {
-      return String (defaultValue);
+    } catch(err) {
+      return String(defaultValue);
     }
   });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },[value, key]);
+  
+  return [value, setValue];
 }
