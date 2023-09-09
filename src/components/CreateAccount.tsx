102d5c5b3dfa29a0a8e0cf0260bb4185e60ab2df
vinischmitt <118611384+vinischmitt@users.noreply.github.com>
2023-09-08 21:19:54 -0300

    1 file added

---
+++ src/components/CreateAccount.tsx
@@ -0,0 +1,44 @@
+import { FormEvent, useState} from 'react';
+
+
+import { signIn } from '../Services/firebase';
+
+
+export function Login() {
+  const [usuario, setUsuario] = useState('')
+  const [senha, setSenha] = useState('');
+
+    function handleLogin(e:FormEvent){
+      e.preventDefault();
+      signIn( usuario, senha)
+      .then((credential) => {
+        alert('Bem-vindo!' + credential.user.uid);
+      })
+      .catch((error) => {
+        alert('Usuario ou senha incorretos!');
+      });
+    }
+  
+  return (
+    <>
+    <form onSubmit={handleLogin}>
+      <div>
+      <label> Usuário: </label>
+      <input onChange={(e) => setUsuario(e.target.value)} value={usuario}/>
+      </div>
+  
+      <div>
+        <label>Senha: </label>
+        <input onChange={(e) => setSenha(e.target.value)} value={senha} type="password"/>
+        </div>
+
+        <div>
+          <button type='submit'>Acessar</button>
+          </div>
+          <div>Ainda não tem conta?
+          <button>Que tal criar uma!</button>
+          </div>
+    </form>
+    </>
+  )
+}
