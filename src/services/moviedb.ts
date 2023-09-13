55d8252c77dabe4a6d3a2bcfa930baefbb29b0e5
vinischmitt <118611384+vinischmitt@users.noreply.github.com>
2023-09-13 19:59:38 -0300

    1 file modified

--- src/services/moviedb.ts
+++ src/services/moviedb.ts
@@ -1,9 +1,14 @@
-const endpoint = "http://api.themoviedb.org/v3";
+const endpoint = 'https://api.themoviedb.org/v3';
 const token = '';
 
 const config = {
   headers: {
-    Autorization: 'Bearer ' + token,
+    Autorization: 'Bearer ' + token
   },
 };
 
+export async function getDiscoverMovies(){
+  const response = await fetch(endpoint + "/discover/movie", config);
+  const data = response.json();
+  return data;
+}
