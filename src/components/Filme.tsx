26c2fc4adfcdb458344f5480835c64ea300d2d73
vinischmitt <118611384+vinischmitt@users.noreply.github.com>
2023-09-13 20:37:06 -0300

    1 file modified

--- src/components/Filme.tsx
+++ src/components/Filme.tsx
@@ -1,18 +1,17 @@
 export interface FilmeInterface {
-  original_tittle: string;
+  original_title: string;
   release_date: string;
   poster_path: string;
 }
-
 export interface FilmeProps {
   filme: FilmeInterface;
 }
 
-export function Filme({ filme }: FilmeProps) {
-  return (
-    <>
-    <img src={'https://image.tmdb.org/t/p/w154/' + filme.poster_path} />
-    <div>{filme.original_tittle} </div>
-    </>
+export function Filme({filme} : FilmeProps){
+  return (  
+  <>
+  <img src={"https://image.tmdb.org/t/p/w154" + filme.poster_path} />
+  <div> {filme.original_title}</div>
+  </>
   );
 }
