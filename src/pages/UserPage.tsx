b165f433543c094d7c8ae054340dd37f8dfeb509
vinischmitt <118611384+vinischmitt@users.noreply.github.com>
2023-09-13 19:54:17 -0300

    1 file modified, 1 added

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -2,7 +2,12 @@ export function UserPage()
 {
   return (
     <>
-      <h1>Bem Vindo</h1>
+      <h1>Fernet com coca</h1>
+      <hr />
+      <h2>Seus Favoritos</h2>
+      <hr />
+      <h2>Veja outros lançamentps</h2>
+      <hr />
     </>
   );
 } 
---
+++ src/services/moviedb.ts
@@ -0,0 +1,9 @@
+const endpoint = "http://api.themoviedb.org/v3";
+const token = '';
+
+const config = {
+  headers: {
+    Autorization: 'Bearer ' + token,
+  },
+};
+
