ab2179546ded2fb4890fb7f32bd7208e5570f883
vinischmitt <118611384+vinischmitt@users.noreply.github.com>
2023-09-13 20:06:14 -0300

    1 file modified

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,12 +1,26 @@
-export function UserPage() 
-{
+import { useEffect, useState } from 'react';
+import { getDiscoverMovies } from '../services/moviedb';
+
+export function UserPage() {
+  const [movies, setMovies] = useState([]);
+
+  async function handleLoadMovies() {
+    const data = await getDiscoverMovies();
+    setMovies(data.results);
+  }
+
+  useEffect(() => {
+    handleLoadMovies();
+  }, []);
+
+
   return (
     <>
       <h1>Fernet com coca</h1>
       <hr />
       <h2>Seus Favoritos</h2>
       <hr />
-      <h2>Veja outros lançamentps</h2>
+      <h2>Veja outros lançamentos</h2>
       <hr />
     </>
   );
