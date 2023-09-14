f8811fd5bd3ab6af251efa01f6529fd049438ed4
vinischmitt <118611384+vinischmitt@users.noreply.github.com>
2023-09-13 21:40:00 -0300

    2 files modified

--- src/components/Filme/Filme.tsx
+++ src/components/Filme/Filme.tsx
@@ -15,8 +15,12 @@ export function Filme({filme} : FilmeProps){
   <>
   <div className="filme">
   <img src={"https://image.tmdb.org/t/p/w154" + filme.poster_path}/>
-  <small>{filme.vote_average} </small>  
-  <div> {filme.original_title}</div>
+  <small>{filme.vote_average} </small> 
+  <button>+ adicionar </button>
+  <div> 
+   <b> {filme.original_title} </b>
+    {filme.release_date}
+  </div>
   </div>
   </>
   );
